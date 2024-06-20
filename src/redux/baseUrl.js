2eb80589f7e0335286fc4d85ef17984496649628
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-06-20 04:27:12 +0300

    v2.0.0

--- src/redux/baseUrl.js
+++ src/redux/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:4001/api/v2/"
-// export const baseUrl = "https://backend-v2.netlify.app/api/v2/";
+// export const baseUrl = "http://localhost:4001/api/v2/"
+export const baseUrl = "https://superback-end.onrender.com/api/v2/";
