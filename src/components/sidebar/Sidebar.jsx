bf0120aa001634e82638c6baf420c03dcb852882
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-06-21 21:48:50 +0300

    v2.0.0

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sidebar.css";
 import logo from "../../image/hamaloğlu-Photoroom.png-Photoroom.png";
 import { RiDashboardFill } from "react-icons/ri";
@@ -7,55 +7,90 @@ import { AiFillProduct, AiOutlineSubnode } from "react-icons/ai";
 import { TbCategoryPlus } from "react-icons/tb";
 import { FaSquarePlus } from "react-icons/fa6";
 import { SiGooglepubsub } from "react-icons/si";
+import { RiMenu2Fill } from "react-icons/ri";
+import { MdClose } from "react-icons/md";
 
 const Sidebar = () => {
+  const [isMenu, setIsmenu] = useState(true);
+
   return (
-    <aside className="__header">
-      <div className="head">
-        <img src={logo} alt="logo" />
-        <p>Orhan TOK</p>
-        <p className="admin">Admin</p>
-      </div>
-      <div className="menu">
-        <ul>
-          <li>
-            <RiDashboardFill />
-            <p>Dashboard</p>
-          </li>
-          <li>
-            <AiFillProduct />
-            <p>Ürünler</p>
-          </li>
-          <li>
-            <TbCategoryPlus />
-            <p>Ürün Ekle</p>
-          </li>
-          <li>
-            <MdCategory />
-            <p>Kategori</p>
-          </li>
-          <li>
-            <FaSquarePlus />
-            <p>Kategori Ekle</p>
-          </li>
-        </ul>
+    <aside
+      className="__header"
+      style={{ boxShadow: isMenu ? "0px 0px 2px whitesmoke" : "none" }}
+    >
+      <RiMenu2Fill
+        className="menu_icon"
+        style={{
+          display: isMenu ? "none" : "block",
+        }}
+        onClick={() => setIsmenu(!isMenu)}
+      />
 
-        <ul>
-          <li>
-            <SiGooglepubsub />
-            <p>Şubeler</p>
-          </li>
-          <li>
-            <AiOutlineSubnode />
-            <p>Şube Ekle</p>
-          </li>
-        </ul>
+      <div
+        className="sidebar"
+        style={{
+          display: isMenu ? "block" : "none",
+        }}
+      >
+        <div className="top_menu">
+          <div className="head">
+            <img src={logo} alt="logo" />
+            <div className="txt">
+              <p>Orhan TOK</p>
+              <span className="admin">Admin</span>
+            </div>
+          </div>
+          <MdClose
+            style={{
+              display: isMenu ? "block" : "none",
+            }}
+            className="close"
+            onClick={() => setIsmenu(!isMenu)}
+          />
+        </div>
+        <div className="menu">
+          <ul>
+            <li>
+              <RiDashboardFill />
+              <p>Dashboard</p>
+            </li>
+            <li>
+              <AiFillProduct />
+              <p>Ürünler</p>
+            </li>
+            <li>
+              <TbCategoryPlus />
+              <p>Ürün Ekle</p>
+            </li>
+            <li>
+              <MdCategory />
+              <p>Kategori</p>
+            </li>
+            <li>
+              <FaSquarePlus />
+              <p>Kategori Ekle</p>
+            </li>
+            <span></span>
+          </ul>
 
-        <div className="logout">
-          <li>
-            <MdModeStandby />
-            <p>Çıkış Yap</p>
-          </li>
+          <ul>
+            <li>
+              <SiGooglepubsub />
+              <p>Şubeler</p>
+            </li>
+            <li>
+              <AiOutlineSubnode />
+              <p>Şube Ekle</p>
+            </li>
+            <span></span>
+          </ul>
+
+          <div className="logout">
+            <li>
+              <MdModeStandby />
+              <p>Çıkış Yap</p>
+            </li>
+          </div>
         </div>
       </div>
     </aside>
