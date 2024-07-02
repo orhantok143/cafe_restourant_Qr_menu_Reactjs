005575cc87f5028cbb2c6b4b7b3d9414a77ef4e7
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-07-02 04:03:13 +0300

    2.0.0

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,45 +1,38 @@
 import React, { useState } from "react";
 import "./list.css";
 import { NavLink } from "react-router-dom";
-import { IoIosArrowBack } from "react-icons/io";
-import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { IoAddCircle } from "react-icons/io5";
 import { useSelector } from "react-redux";
 
-const List = ({ headers, itemsPerPage }) => {
-  const items = [
-    { Ad: "Ürün 1", Soyad: "Detay 1", Yaş: "20", Meslek: "Kategori 1" },
-    { Ad: "Ürün 2", Soyad: "Detay 2", Yaş: "30", Meslek: "Kategori 2" },
-    // Daha fazla öğe ekleyin
-  ];
+const List = ({ headers, itemsPerPage, items, head }) => {
   const addingProduct = useSelector((state) => state.products.loading);
   const [pageNumber, setPageNumber] = useState(0);
 
+  // Sayfada gösterilecek öğeleri hesaplama
   const pagesVisited = pageNumber * itemsPerPage;
+  const displayItems =
+    items?.length > 5
+      ? items.slice(pagesVisited, pagesVisited + itemsPerPage)
+      : items;
+  const pageCount = Math.ceil(items?.length / itemsPerPage);
 
-  const displayItems = items
-    .slice(pagesVisited, pagesVisited + itemsPerPage)
-    .map((item, index) => (
-      <tr key={index}>
-        {headers.map((header, colIndex) => (
-          <td key={colIndex}>{item[header]}</td>
-        ))}
-      </tr>
-    ));
-
-  const pageCount = Math.ceil(items.length / itemsPerPage);
-
+  // Önceki sayfaya geçiş
   const handlePrevious = () => {
     if (pageNumber > 0) {
       setPageNumber(pageNumber - 1);
     }
   };
 
+  // Sonraki sayfaya geçiş
   const handleNext = () => {
     if (pageNumber < pageCount - 1) {
       setPageNumber(pageNumber + 1);
     }
   };
+
+  console.log(displayItems);
+  console.log(head);
 
   return (
     <div className="list-container">
@@ -51,19 +44,46 @@ const List = ({ headers, itemsPerPage }) => {
             ))}
           </tr>
         </thead>
-        <tbody>{displayItems}</tbody>
+        <tbody>
+          {head === "Kategoriler"
+            ? displayItems.map((category) => (
+                <React.Fragment key={category._id}>
+                  {category.subCategory?.map((sub) => (
+                    <tr key={sub._id}>
+                      <td>{category.title}</td>
+                      <td>{sub.title}</td>
+                    </tr>
+                  ))}
+                </React.Fragment>
+              ))
+            : displayItems?.map((item, key) => (
+                <tr key={key}>
+                  {head === "Ürünler" ? (
+                    <>
+                      <td>{item.name}</td>
+                      <td>{item.category}</td>
+                      <td>{item.price} TL</td>
+                    </>
+                  ) : head === "Şubeler" ? (
+                    <>
+                      <td>{item.name}</td>
+                      <td>{item.owner}</td>
+                    </>
+                  ) : null}
+                </tr>
+              ))}
+        </tbody>
       </table>
       <div className="pagination-container">
         <IoIosArrowBack
           className="pagination-button"
           onClick={handlePrevious}
         />
-
         <IoIosArrowForward className="pagination-button" onClick={handleNext} />
       </div>
       <NavLink to="../add-product" className="add-item-button add-product">
         <IoAddCircle />
-        {addingProduct ? "ürün Ekleniyor" : "Ekle"}
+        {addingProduct ? "Ürün Ekleniyor" : "Ekle"}
       </NavLink>
     </div>
   );
--- src/components/middlesection/Main.jsx
+++ src/components/middlesection/Main.jsx
@@ -1,17 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./main.css";
 import { AiFillProduct } from "react-icons/ai";
 import { MdCategory } from "react-icons/md";
 import { SiGooglepubsub } from "react-icons/si";
 import { FaCommentDots } from "react-icons/fa6";
 import List from "../list/List";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectActiveBusinesses,
+  selectActiveCategories,
+  selectActiveProducts,
+} from "../../redux/selectors";
+import { useParams } from "react-router-dom";
+import { getAllProducts } from "../../redux/products/productSlice";
+import { getAllCategories } from "../../redux/category/categorySlice";
+import { getAllBusinesses } from "../../redux/businesses/businessesSlice";
 
 const Main = () => {
-  const headers = ["Ad", "Soyad", "Yaş", "Meslek"];
+  const dispatch = useDispatch();
+  const param = useParams();
+  const [whichCard, setwhichCard] = useState("Product");
+  const products = useSelector(selectActiveProducts);
+  const categories = useSelector(selectActiveCategories);
+  const branch = useSelector(selectActiveBusinesses);
+  const productList = {
+    head: "Ürünler",
+    header: ["ÜRÜN İSMİ", "ANA KATEGORİ", "FİYAT"],
+    items: products.products,
+  };
+  const categoryList = {
+    head: "Kategoriler",
+    header: ["KATEGORİ İSMİ", "ALT KATEGORİLER"],
+    items: categories.categories,
+  };
+  const branchList = {
+    head: "Şubeler",
+    header: ["İŞLETME İSMİ", "ŞUBELER"],
+    items: branch?.businesses?.filter((branch) => branch._id === param.id),
+  };
+  const commentList = {
+    head: "Yorumlar",
+    header: ["YORUM YAPAN", "ÜRÜN", "YORUM"],
+  };
+  const handleOnClick = (data) => {
+    setwhichCard(data);
+  };
+  const [header, setHeader] = useState(productList);
+
+  useEffect(() => {
+    if (whichCard === "Product") {
+      setHeader(productList);
+    }
+    if (whichCard === "Category") {
+      setHeader(categoryList);
+    }
+    if (whichCard === "Comment") {
+      setHeader(commentList);
+    }
+    if (whichCard === "Branch") {
+      setHeader(branchList);
+    }
+  }, [whichCard]);
+
+  useEffect(() => {
+    if (!products?.products) {
+      dispatch(getAllProducts());
+    }
+    if (!categories?.categories) {
+      dispatch(getAllCategories());
+    }
+    if (branch?.bussiness) {
+      dispatch(getAllBusinesses());
+    }
+  }, []);
+
   return (
     <main className="main">
       <div className="cards">
-        <div className="card">
+        <div className="card" onClick={() => handleOnClick("Product")}>
           <div className="up_card">
             <AiFillProduct />
             <p>Ürünler</p>
@@ -21,7 +87,7 @@ const Main = () => {
             <span>100</span>
           </div>
         </div>
-        <div className="card">
+        <div className="card" onClick={() => handleOnClick("Category")}>
           <div className="up_card">
             <MdCategory />
             <p>Kategoriler</p>
@@ -31,7 +97,7 @@ const Main = () => {
             <span>100</span>
           </div>
         </div>
-        <div className="card">
+        <div className="card" onClick={() => handleOnClick("Branch")}>
           <div className="up_card">
             <SiGooglepubsub />
             <p>Şubeler</p>
@@ -41,7 +107,7 @@ const Main = () => {
             <span>100</span>
           </div>
         </div>
-        <div className="card">
+        <div className="card" onClick={() => handleOnClick("Comment")}>
           <div className="up_card">
             <FaCommentDots />
             <p>Yorumlar</p>
@@ -54,7 +120,7 @@ const Main = () => {
       </div>
       <div className="lists">
         <div className="list_header">
-          <h1>Ürünler</h1>
+          <h1> {header.head} </h1>
           <div className="controlbar">
             <input type="search" placeholder="Ara..." />
             {/* <div className="categoryControl">
@@ -74,7 +140,12 @@ const Main = () => {
             </div> */}
           </div>
         </div>
-        <List headers={headers} itemsPerPage={5} />
+        <List
+          headers={header.header}
+          itemsPerPage={5}
+          items={header.items}
+          head={header.head}
+        />
       </div>
     </main>
   );
