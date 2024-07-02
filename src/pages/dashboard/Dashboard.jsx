b0b6dc5cddf99e1e513beb8f81a6a114366c20ec
orhantok143 <119065177+orhantok143@users.noreply.github.com>
2024-07-02 21:33:10 +0300

    v2.0.0

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./dashboard.css";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Main from "../../components/middlesection/Main";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Leftbar from "../../components/leftbar/Leftbar";
 import { useParams } from "react-router-dom";
 import ProductList from "../../components/productList/ProductList";
 import AddProduct from "../../components/addProduct/AddProduct";
 import AddCategory from "../../components/addCategory/AddCategory";
 import CategoryList from "../../components/categoryList/CategoryList";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProducts } from "../../redux/products/productSlice";
+import { getAllCategories } from "../../redux/category/categorySlice";
+import { checkToken } from "../../redux/login/loginSlice";
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const param = useParams();
+  const loginState = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+    dispatch(getAllCategories());
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(checkToken(token));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (loginState.tokenValid) {
+      navigate(`/${param.id}/anasayfa`);
+    } else {
+      navigate(`/${param.id}/login`);
+    }
+  }, [navigate, param.id, loginState.tokenValid]);
+
   return (
     <div className="container">
       <Sidebar param={param} />
