0c050fce58e5f10d1bfc532c82bc52eccb4839a0
orhantok143 <orhantok143@gmail.com>
2024-08-24 02:09:04 +0300

    state updated

--- src/redux/baseUrl.js
+++ src/redux/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:4001/api/v2/"
-// export const baseUrl = "https://cafeapiv2-b15257aa7263.herokuapp.com/api/v2/"
+// export const baseUrl = "http://localhost:4001/api/v2/"
+export const baseUrl = "https://cafeapiv2-b15257aa7263.herokuapp.com/api/v2/"
